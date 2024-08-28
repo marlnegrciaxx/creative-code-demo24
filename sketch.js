54a6cb794885b06c63a3d3926587331d1d112410
marlnegrciaxx <168107812+marlnegrciaxx@users.noreply.github.com>
2024-08-28 13:43:02 -0500

    add all the files

---
+++ sketch.js
@@ -0,0 +1,15 @@
+console.log("this sketch is from sketch.js")
+console.log("this is my first website")
+
+function setup(){
+    createCanvas(400,400)
+
+    background (100,100);
+}
+function draw() {
+  
+}
+
+function mouseMoved(){
+    rect(mouseX,mouseY,20,20);
+}
